b4fac2ad5647a72fe4e1cc7b5f3c414abaec9c33
SamalaSumanth0262 <chintusamala96@gmail.com>
2024-10-29 09:01:49 +0000

    browse page init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-// App.js
-import React from 'react';
 import NetflixTitle from './NetflixTitle';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <Router>
       <NetflixTitle />
-    </div>
+    </Router>
   );
 }
 
--- src/NetflixTitle.tsx
+++ src/NetflixTitle.tsx
@@ -1,16 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './NetflixTitle.css';
 import netflixSound from './netflix-sound.mp3';
-
+import { useNavigate } from 'react-router-dom';
 const NetflixTitle = () => {
   const title = "Sumanth Samala";
   const [isClicked, setIsClicked] = useState(false);
+  const navigate = useNavigate();
 
   const handlePlaySound = () => {
     const audio = new Audio(netflixSound);
     audio.play().catch(error => console.error("Audio play error:", error));
     setIsClicked(true); // Starts animation after clicking
   };
+
+
+  useEffect(() => {
+    if (isClicked) {
+      const timer = setTimeout(() => {
+        navigate('/browse');
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [isClicked, navigate]);
 
   return (
     <div className="netflix-container" onClick={handlePlaySound}>
